b89a72b4dbb6a47d8d99c74f2a42b5bc5baf00da
kgallagher52 <keatonrgallagher@gmail.com>
2023-04-10 14:43:11 -0600

    feat(running-application): basic start code

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const electron = require('electron');
+const { app } = require('electron');
 
+// app is the overall running process 
+
+/***
+ * Electron lifecycle process created by app
+ *  Electron Starts
+ *  App Process is created 
+ *  App Ready to Star doing things
+ *  
+ *  App closes down
+ */
+
+// Event handler to handle starting application
+app.on('ready', () => {
+  console.log("App is ready");
+});
