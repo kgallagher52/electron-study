b4f4ce4c28e08da0ef406db4c039ea3ba6334d00
kgallagher52 <keatonrgallagher@gmail.com>
2023-04-12 16:33:06 -0600

    feat(resize-top-bar): removed top bar on window and made it ubable to resize

--- tasky/index.js
+++ tasky/index.js
@@ -4,6 +4,7 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
+    width: 1200, height: 800, // This is just a custom size for my use
     x: 2500, y: 0, // Tell where on the users screen to put the app
     webPreferences: {
       // --- !! IMPORTANT !! ---
@@ -11,10 +12,14 @@ function createWindow() {
       // 'contextIsolation' defaults to "true" as from Electron v12
       contextIsolation: false,
       nodeIntegration: true
-    }
+    },
+    // Remove status bar at the top
+    frame: false,
+    // Remove the ability to resize the window
+    resizable: false,
   })
-  mainWindow.loadFile('./src/index.html');
-  mainWindow.webContents.openDevTools();
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+  // mainWindow.webContents.openDevTools();
   mainWindow.on('closed', () => app.quit());
 }
 
